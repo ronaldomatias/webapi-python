910dbe09f217a377c014697493ad754c71958e60
Ronaldo Matias <ronaldomatias49@hotmail.com>
2022-07-04 19:20:10 -0300

    subindo projeto

---
+++ Classes/pessoa.py
@@ -0,0 +1,8 @@
+class Pessoa:
+    #Método Construtor
+    def __init__(self, nome, idade, solteiro):
+        self.nome = nome
+        self.idade = idade
+        self.solteiro = solteiro
+
+
---
+++ main.py
@@ -0,0 +1,5 @@
+from Classes.pessoa import Pessoa
+
+if __name__ == '__main__':
+    p = Pessoa('Ronaldo', 25, True)
+    print(p.nome)
